3eaf3a483fc71dc2449cb0ea8337d850478d1c64
Sabbadini10 <matiassabbadini24@gmail.com>
2023-01-01 18:37:34 -0300

    cambie archivo config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: root,
+    password: "root",
+    database: buonaseo_db,
+    host: '0.0.0.0',
+    port: 3306,
     dialect: "mysql",
     logging: false,
   },
